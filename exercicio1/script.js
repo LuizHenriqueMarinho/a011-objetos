d6bf754d0c618687dc106d2343fbc74a55baf395
LuizHenriqueMarinho <luizhenriquemarinhofga@gmail.com>
2022-09-05 13:53:02 -0300

    exercicios a011 resolvidos

---
+++ exercicio1/script.js
@@ -0,0 +1,39 @@
+// # Exercício 1
+
+// Crie um array vazio chamado `sacolao`, e siga os passos abaixo:
+
+// a) Crie três objetos que vão representar frutas do sacolão. Os objetos devem ter as seguintes propriedades: 
+
+// ```jsx
+// nome: string;
+// preco: number;
+// disponibilidade: boolean;
+// ```
+
+// b) Adicione os objetos ao array `sacolao` utilizando o método **`push()`**
+
+// c) Imprima a o array `sacolao`, e certifique-se de que agora ela seja um array **com três objetos**
+
+const sacolao = []
+
+produto_1 = 
+{nome : "cenoura" , preco : 10 , disponibilidade : true }
+
+produto_2 =
+{nome : "tomate" , preco : 15 , disponibilidade : false } 
+
+produto_3 =
+{nome : "batata" , preco : 20 , disponibilidade : true }
+
+sacolao.push(produto_1)
+sacolao.push(produto_2)
+sacolao.push(produto_3)
+
+console.log(sacolao)
+
+
+
+
+
+
+
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,3 +1,15 @@
+// # Exercício 2
+
+// Leia o código abaixo, e responda:
+
+// a) Qual o código para imprimir o nome do **primeiro** ator/atriz?
+
+// b) Qual o código para imprimir o nome do **último** ator/atriz?
+
+// c) Qual o código para exibir o array com **todas** as transmissões de hoje?
+
+// d) Qual o código para exibir o **horário** de transmissão do filme no `Canal Brasil`?
+
 const filme = {
 	nome: "Auto da Compadecida", 
 	ano: 2000, 
@@ -12,4 +24,15 @@ const filme = {
 		]
 };
 
-// respostas aqui ⬇️
+// respostas aqui ⬇
+//a)
+console.log(filme.elenco[0])
+
+//b)
+console.log(filme.elenco[3])
+
+//c)
+console.log(filme.transmissoesHoje)
+
+//d)
+console.log(filme.transmissoesHoje[1].horario)
